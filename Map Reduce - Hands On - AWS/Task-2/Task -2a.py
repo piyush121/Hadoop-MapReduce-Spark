d67ed8cf7797a280b6e29f2b808eb9a1826f32a3
piyush121 <piyushchaudhary07@gmail.com>
2016-04-13 14:27:31 -0400

    My 100th straight day of coding - Feeling Great

---
+++ Map Reduce - Hands On - AWS/Task-2/Task -2a.py
@@ -0,0 +1,27 @@
+import sys
+
+for line in sys.stdin:
+    line = line.strip().split("\t")
+    line = line.split(",")
+    fareAmount = line[9]
+
+    if 0 <= fareAmount <= 4:
+        print "%i, %i\t%i" % (0, 4, 1)
+    if 4.01 <= fareAmount <= 8:
+        print "%i, %i\t%i" % (0, 8, 1)
+    if 8.01 <= fareAmount <= 12:
+        print "%i ,%i\t%i" % (8.01, 12, 1)
+    if 12.01 <= fareAmount <= 16:
+        print "%i, %i\t%i" % (12.01, 16, 1)
+    if 16.01 <= fareAmount <= 20:
+        print "%i, %i\t%i" % (16.01, 20, 1)
+    if 20.01 <= fareAmount <= 14:
+        print "%i, %i\t%i" % (20.01, 24, 1)
+    if 24.01 <= fareAmount <= 48:
+        print "%i, %i\t%i" % (24.01, 48, 1)
+
+    if 0 <= fareAmount <= sys.maxint:
+        print "%i, infinite\t%i" % (0, 1)
+
+
+
