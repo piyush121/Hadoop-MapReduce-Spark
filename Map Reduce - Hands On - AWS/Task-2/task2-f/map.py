3fe42f4011c1253081ff59f36ea7f162e878c137
piyush121 <piyushchaudhary07@gmail.com>
2016-04-14 01:11:06 -0400

    From mapper --to --> reducer on Hadoop.

---
+++ Map Reduce - Hands On - AWS/Task-2/task2-f/map.py
@@ -0,0 +1,7 @@
+import sys
+
+for line in sys.stdin:
+    line = line.strip().split("\t")[0].split(",")
+    hackLicense = line[1]
+    medallion = line[0]
+    print "%s\t%s" % (hackLicense, medallion)
---
+++ Map Reduce - Hands On - AWS/Task-2/task2-f/reduce.py
@@ -0,0 +1,23 @@
+import sys
+
+prevKey = ""
+prevCar = ""
+count = 0
+for line in sys.stdin:
+    line = line.strip().split("\t")
+    currKey = line[0]
+    currCar = line[1]
+    if prevKey == "":
+        prevKey = currKey
+        prevCar = currCar
+        count = 1
+        continue
+    if currKey == prevKey:
+        if currCar != prevCar:
+            count += 1
+    else:
+        print "%s\t%i" % (prevKey, count)
+        prevKey = currKey
+        prevCar = currCar
+        count = 1
+print "%s\t%i" % (prevKey, count)
