954b64dedecddb5cc9379907b759ebb6f5726b53
piyush121 <piyushchaudhary07@gmail.com>
2016-04-14 11:56:31 -0400

    MapR and more MapR

--- Map Reduce - Hands On - AWS/Task-3/map.py
+++ Map Reduce - Hands On - AWS/Task-3/map.py
@@ -1,17 +1,17 @@
+import StringIO
+import csv
 import sys
 
+
 for line in sys.stdin:
-
-    line = line.strip()
-
-    if len(line) == 263:
-        line = line.split("\t")
-        left = line[0].split(",")
-        key = left[0]
-        leftr = "".join(left[1:])
-        print "%s\t%s,%s" % (key, leftr, line[1])
-    elif len(line) == 185:
-        line = line.split(",")
-        key = line[0]
-        rem = "".join(line[1:])
-        print "%s\t%s" % (key, rem)
+    csv_file = StringIO.StringIO(line)
+    csv_reader = csv.reader(csv_file)
+    for record in csv_reader:
+        if 'MEDALLION' == record[2]:
+            key = record[0]
+            rem = ",".join(record[1:])
+            print "%s\t%s%s" % (key, '1', rem)
+        else:
+            key = record[0]
+            rem = ",".join(record[1:]).replace("\t", ",")
+            print "%s\t%s%s" % (key, '0', rem)
