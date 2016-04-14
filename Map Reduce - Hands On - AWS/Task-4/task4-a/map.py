0a5e98cec71585d7f0b3f29e751cbb4eafb188f7
piyush121 <piyushchaudhary07@gmail.com>
2016-04-14 16:18:52 -0400

    MapR and more MapR

---
+++ Map Reduce - Hands On - AWS/Task-4/task4-a/map.py
@@ -0,0 +1,13 @@
+import StringIO
+import csv
+import sys
+
+
+for record in sys.stdin:
+    csv_file = StringIO.StringIO(record)
+    csv_reader = csv.reader(csv_file)
+    for line in csv_reader:
+        key = line[25]
+        revenue = float(line[14]) + float(line[15]) + float(line[17])
+        tip = float(line[17])
+        print "%s\t%f,%f" % (key, revenue, tip/revenue)
